63719d9cacc98d3bce95fd0b1ab5e23eb1673302
chaate <chaate@163.com>
2017-04-24 11:08:09 +0800

    增加欢迎界面，增加预报数据

---
+++ app/src/main/java/com/dulikaifa/zhitianweather/bean/HourForecast.java
@@ -0,0 +1,11 @@
+package com.dulikaifa.zhitianweather.bean;
+
+/**
+ * Author:李晓峰 on 2017/4/23 23:27
+ * E-mail:chaate@163.com
+ * Copyright(c)2017,All rights reserved.
+ * Usage :
+ */
+
+public class HourForecast {
+}
